24a7e1b4731762d118938b90a3c62aaf54bfad95
Brian O'Shea <oshea@msu.edu>
2026-02-20 05:53:31 -0500

    Stuff for homework 2

--- homework/hw_2/FOR_NEXT_TIME.md
+++ homework/hw_2/FOR_NEXT_TIME.md
@@ -1,6 +1,6 @@
 # Homework #2 
 
-For next time:
+Spring 2023 -- For next time:
 
 * make sure to tell students to put things in Python text files, not notebooks!  Also make it clear that the code needs to look OK for PyLint.
 
@@ -16,3 +16,7 @@ Make sure to ask students to report how much time they spent on each part.
 
 For the gravitational potential question, make sure to give the students a hint about units - if G is in CGS, dx and rho have to be in CGS as well!  (and the make_star_array method does return the stellar densities in CGS!)
 
+
+Spring 2026 -- for next time:
+
+...
--- homework/hw_2/star_array_generator.py
+++ homework/hw_2/star_array_generator.py
@@ -1,3 +1,8 @@
+"""
+Code for part 2 of homework 2 - the function's doc string
+describes the behavior.
+include this with ``from star_array_generator import make_star_array''
+"""
 
 def make_star_array(Ngrid=103,Nstars=1,Lbox=1.0,Mstar = 1.0):
     '''
@@ -17,7 +22,7 @@ def make_star_array(Ngrid=103,Nstars=1,Lbox=1.0,Mstar = 1.0):
     
     '''
     import numpy as np
-    
+
     # "stellar density" - Mstar / V_cell in CGS units
     rho_star = Mstar*1.989e33/(Lbox/float(Ngrid-2)*3.0857e+18)**3
 
@@ -28,22 +33,24 @@ def make_star_array(Ngrid=103,Nstars=1,Lbox=1.0,Mstar = 1.0):
     many_star_array = np.zeros([Ngrid,Ngrid,Ngrid])
 
     if Nstars==1:
-        # get the center cell using integer division 
+        # get the center cell using integer division
         cen = Ngrid // 2
 
         # make array with a single star
         single_star_array[cen,cen,cen] += rho_star
 
         return single_star_array
-    else:
-        # set random seed so result is reproducible
-        np.random.seed(8675309)
 
-        # loop over many_star_array and distribute stars 
-        # randomly, but not too close to the edge.
-        for i in range(Nstars):
-            many_star_array[np.random.randint(10,Ngrid-10),
-                                np.random.randint(10,Ngrid-10),
-                                np.random.randint(10,Ngrid-10)] += rho_star
+    # if Nstars != 1 generate a bunch of stars!
 
-        return many_star_array
+    # set random seed so result is reproducible
+    np.random.seed(8675309)
+
+    # loop over many_star_array and distribute stars
+    # randomly, but not too close to the edge.
+    for i in range(Nstars):
+        many_star_array[np.random.randint(10,Ngrid-10),
+                           np.random.randint(10,Ngrid-10),
+                           np.random.randint(10,Ngrid-10)] += rho_star
+
+    return many_star_array
