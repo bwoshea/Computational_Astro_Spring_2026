a16fbe49397b689c0a22c0b6f5634271a3d6f0cb
Brian O'Shea <oshea@msu.edu>
2026-03-07 07:20:37 -0500

    updating homework 4

--- homework/hw_4/luminosity_schechter.py
+++ homework/hw_4/luminosity_schechter.py
@@ -42,26 +42,57 @@ def luminosity_function(nbins=10, error_scale=1,
     # calculate width of magnitude bin
     dM = (abs_mag_max-abs_mag_min)/nbins
 
-    # calculate magnitude bins
+    # create magnitude bins
     mag_bins = np.arange(abs_mag_min,abs_mag_max,dM)
 
     # calculate number density for those magnitude bins
     number_density = 0.4*np.log(10.0) * phi*((10**(0.4*(mag_star-mag_bins)))**(alpha+1)) \
                       * np.exp(-10**(0.4*(mag_star-mag_bins)))
 
-    # calculate error bars.  Start out simply.
+
+    '''
+    Create error estimate.  We're using a very simple error estimate
+    to start out, which assumes that the error is linearly
+    proportional to the number density of galaxies in a given bin.
+    This is completely indefensible for a real galaxy survey!
+    '''
     error_bars = error_scale*0.1*number_density
 
-    # The following lines are meant to modify the error estimates so that brighter galaxies
-    # (i.e., those with more negative magnitudes) have larger error bars proportionally to their
-    # values.  This is because there are fewer galaxies in those bins so there are large Poisson
-    # errors.  Be very careful changing this!
 
-    ######  DO NOT USE THIS IT IS BROKEN AND GARBAGE ########
-    # error_bars *= np.abs(1.0+np.random.normal(loc=0.0,scale=.2))
-    # error_pivot_point=mag_star+4.0
-    # error_modifier = 10**(np.abs((mag_bins/error_pivot_point))**2-1)
-    # error_bars *= error_modifier
+    '''
+    Create your own error estimates here. Galaxy surveys are typically
+    limited by some combination of the volume surveyed and by the
+    apparent magnitude of galaxies. More massive galaxies are both
+    much brighter (and thus easier to find) and also have lower number
+    density (and are thus rarer) than their low-mass counterparts,
+    which are dim but numerous.
+
+    This means that a deep (in magnitude) but volume-limited galaxy
+    survey will be relatively "complete" at the low-mass end (meaning
+    the low-luminosity galaxies have all been found and counted, so
+    the error bars are small for those bins) but covers a small
+    volume, meaning the number of high mass galaxies are small and
+    thus the number density estimates are statistically limited (and
+    thus the error bars are large).  A shallow but large-volume galaxy
+    survey will have exactly the opposite problem - the counting of
+    bright galaxies will be robust (i.e., the corresponding bins will
+    have small error bars) due to the wide area of coverage, but many
+    low-luminosity galaxies will be missed, meaning the corresponding
+    bins will have large error bars. Astronomical surveys are
+    inevitably limited in the amount of telescope time available for
+    the survey, which means that tradeoffs in survey design must be
+    made in order to accommodate these challenges!
+
+    So, when choosing your own estimates for error bars, try setting
+    up errors that approximate both deep, volume-limited and shallow,
+    wide-field surveys.  Try also approximating a "snapshot" survey
+    that is both volume- and magnituded-limited, meaning the error
+    bars at both the high ahd low-luminosity ends are bad.  How do
+    these choices impact the degree to which different parameters in
+    the model are constrained?
+
+    '''
+    # error_bars = UNCOMMENT AND ADD YOUR CODE HERE
 
     return mag_bins, number_density, error_bars
 
